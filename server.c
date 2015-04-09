beee77c2393f36cb0acb20482069937469d644b5
zhengguoqiang <414471826@qq.com>
2015-04-09 16:06:25 +0800

    t

--- server.c
+++ server.c
@@ -45,7 +45,7 @@ int main(void)
 	}
 	server_addr.sin_family = AF_INET;         // host byte order
 	server_addr.sin_port = htons(MYPORT);     // short, network byte order
-	server_addr.sin_addr.s_addr = inet_addr("192.168.0.24");//INADDR_ANY; // automatically fill with my IP
+	server_addr.sin_addr.s_addr = INADDR_ANY;//inet_addr("192.168.0.24"); // automatically fill with my IP
 	memset(server_addr.sin_zero, '\0', sizeof(server_addr.sin_zero));
 	if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
 		perror("bind");
