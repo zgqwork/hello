40d42bd7ccf334e2f5acb216f9ab649abc237c00
zhengguoqiang <414471826@qq.com>
2015-04-13 14:50:29 +0800

    't'

--- us.c
+++ us.c
@@ -1,4 +1,4 @@
-#if 0
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -23,8 +23,9 @@ void do_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen)
 		n = recvfrom(sockfd, mesg, 80, 0, pcliaddr, &len);
 		/* sent data back to client */
 		
-		client_addr = (struct sockaddr_in*)pcliaddr;
-		printf("%s %d", inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port));
+		//client_addr = (struct sockaddr_in*)pcliaddr;
+		//printf("%s %d", inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port));
+		printf ("received packet from %s:\n", inet_ntoa (pcliaddr.sin_addr));
 		
 		sendto(sockfd, mesg, strlen(mesg), 0, pcliaddr, len);
 	}
@@ -53,63 +54,4 @@ int main(void)
 	do_echo(sockfd, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
 
 	return 0;
-}
-#endif
-
-#include<stdio.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
- 
-#define lport 8888
- 
-int main()
-{
-    int mysock,len;
-    struct sockaddr_in addr;
-    int i=0;
-    char msg[256];
- 
-    int addr_len;
- 
-    if((mysock=socket(AF_INET,SOCK_DGRAM,0))<0)
-    {
-        perror("error");
-        exit(1);
-    }
-    else
-    {
-        printf("socket created.\n");
-        printf("socket ID: %d\n",mysock);
-    }
-    addr_len=sizeof(struct sockaddr_in);
-    bzero(&addr,sizeof(addr));
-    addr.sin_family=AF_INET;
-    addr.sin_port=htons(lport);
-    addr.sin_addr.s_addr=htonl(INADDR_ANY);
- 
-    if(bind(mysock,&addr,sizeof(addr))<0)
-    {
-        perror("connect");
-        exit(1);
-    }
-    else
-    {
-        printf("bind ok.\n");
-        printf("local port: %d\n",lport);
-    }
-    while(1)
-    {
-        bzero(msg,sizeof(msg));
-        len=recvfrom (mysock,msg,sizeof(msg),0,&addr,&addr_len);
- 
-        printf("%d: ",i);
-        i++;
-        printf("message form: %s\n",inet_ntoa(addr.sin_addr));
-        printf("message: %s\n\n",msg);
-        sendto(mysock,msg,len,0,&addr,addr_len);
-    }
-    return 0;
 }
