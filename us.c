f565ffb9b020b2ffdca4f6e6ca62fc1af2d195fe
zhengguoqiang <414471826@qq.com>
2015-04-13 15:57:09 +0800

    't'

--- us.c
+++ us.c
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -79,86 +79,53 @@ int main(void)
 
 
 
-#if 0
+#if 1
 #include <stdio.h>
-
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
-
 #define BUFSIZE 1000
-
-int
-
-main (int argc, char *argv[])
-
+int main (int argc, char *argv[])
 {
-
   int s;
-
   int fd;
-
   int len;
-
   struct sockaddr_in my_addr;
-
   struct sockaddr_in remote_addr;
-
   int sin_size;
-
   char buf[BUFSIZE];
 
   //memset (&my_addr, 0, sizeof (my_addr));
 
   my_addr.sin_family = AF_INET;
-
   my_addr.sin_addr.s_addr = INADDR_ANY;
-
   my_addr.sin_port = htons (8888);
-
   if ((s = socket (PF_INET, SOCK_DGRAM, 0)) < 0)
-
-    {
-
-      perror ("socket");
-
-      return 1;
-
-    }
-
-  if (bind (s, (struct sockaddr *) &my_addr, sizeof (struct sockaddr)) < 0)
-
-    {
-
-      perror ("bind");
-
-      return 1;
-
-    }
-
-  sin_size = sizeof (struct sockaddr_in);
-
-  printf ("waiting for a packet...\n");
-
-  for(;;)
   {
-	if ((len = recvfrom (s, buf, BUFSIZE, 0, (struct sockaddr *)&remote_addr, &sin_size)) < 0)
-    {
-      perror ("recvfrom");
-      return 1;
-    }
-    printf("received packet from %s:%d\n", inet_ntoa(remote_addr.sin_addr), ntohs(remote_addr.sin_port));
-	sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&remote_addr, &sin_size);
+	perror ("socket");
+	return 1;
   }
+	if (bind (s, (struct sockaddr *) &my_addr, sizeof (struct sockaddr)) < 0)
+	{
+		perror ("bind");
+		return 1;
+    }
+	sin_size = sizeof (struct sockaddr_in);
+	printf ("waiting for a packet...\n");
+	for(;;)
+	{
+		if ((len = recvfrom (s, buf, BUFSIZE, 0, (struct sockaddr *)&remote_addr, &sin_size)) < 0)
+		{
+			perror ("recvfrom");
+			return 1;
+		}
+		printf("received packet from %s:%d\n", inet_ntoa(remote_addr.sin_addr), ntohs(remote_addr.sin_port));
+		sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&remote_addr, sin_size);
+	}
   /*
   buf[len] = '\0';
-
   printf ("contents:%s\n", buf);
-
   close (s);
   */
   return 0;
